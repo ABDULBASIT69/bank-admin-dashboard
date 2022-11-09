cb52831868372272e46878d25b34658bd529d774
ABDULBASIT69 <chudharyabdulbasit@gmail.com>
2022-11-09 18:24:31 +0500

    Registration form data inserted in firebase

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,56 +3,144 @@ import leftArrow from '../assets/images/left-arrow.png'
 import rightArrow from '../assets/images/right-arrow.png'
 import map from '../assets/images/map.png'
 import logo from '../assets/images/logo.png'
+import {useForm} from 'react-hook-form' 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { storage,db,auth } from '../firebaseConfig';
+import {ref, getDownloadURL, uploadBytesResumable,getMetadata} from 'firebase/storage'
+import {collection,addDoc} from 'firebase/firestore/lite'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 export default function SignUp() {
+    const {register, handleSubmit, watch, formState: { errors }}=useForm()
+    const customId = "custom-id-yes";
+   let details ={}
+    const registration = async (data) =>{
+        data.preventDefault;
+        const storageRef = ref(storage, `files/${data.profileImage[0].name}`);
+        console.log('metadata',storageRef)
+        const uploadTask = uploadBytesResumable(storageRef, data.profileImage[0]);
+        console.log('uploadtask',uploadTask)
+        uploadTask.on('state_changed',
+        (snapShot) => {
+            // console.log('snapshot',snapShot)
+          },(err) => {
+            // console.log('error',err)
+          },() => {
+            getDownloadURL(uploadTask.snapshot.ref)
+             .then(fireBaseUrl => {
+                    addDoc(collection(db,'user-registration'),{
+                    fullname: document.getElementById('fullname').value,
+                    email: document.getElementById('email').value,
+                    birthdate: document.getElementById('birthdate').value,
+                    phonenumber: document.getElementById('phonenumber').value,
+                    province: document.getElementById('province').value,
+                    city: document.getElementById('city').value,
+                    fulladdress: document.getElementById('fullAddress').value,
+                    profileimage: fireBaseUrl,
+                    password: document.getElementById('password').value,
+                }).then((res)=>{
+
+                    toast.success('Successfully Registered!',{
+                                    toastId: customId,
+                                    position: "top-right",
+                                    autoClose: 3000,
+                                    hideProgressBar: false,
+                                    closeOnClick: true,
+                                    pauseOnHover: true,
+                                    draggable: true,
+                                    progress: undefined,
+                                    theme: "colored",
+                                })
+                
+                }).catch((err)=>{
+                    toast.error('Something went Wrong!',{
+                                toastId: customId,
+                                position: "top-right",
+                                autoClose: 3000,
+                                hideProgressBar: false,
+                                closeOnClick: true,
+                                pauseOnHover: true,
+                                draggable: true,
+                                progress: undefined,
+                                theme: "colored",
+                                })
+                })
+             })
+            })
+
+
+        
+        
+    }
     return (
+        <>
+        <ToastContainer />
         <div className="form-bg w-full flex justify-between">
             <div className="form bg-white w-3/5 pt-16 pb-11">
                 <div className="container mx-auto px-6">
                     <h1 className="heading-color">Get started</h1>
-                    <form action="">
+                    <form action="" onSubmit={handleSubmit(registration)}>
                         <div className="pt-5">
                             <span className="gray">Full Name</span>
-                            <div className="input-outline py-5 px-7 my-4"><input type="text" name="fullname" id="fullname" placeholder="Full Name" className="w-full" /></div>
+                            <div className="input-outline py-5 px-7 my-4"><input type="text" name="fullname" id="fullname" {...register('fullName',{required:true})} placeholder="Full Name" className="w-full" /></div>
+                            {errors.fullName && <span className='error-color'>Enter Your Full Name</span>}
                         </div>
                         <div>
                             <span className="gray">Email Address</span>
-                            <div className="input-outline py-5 px-7 my-4"><input type="email" name="email" id="email" placeholder="Email Address" className="w-full" /></div>
+                            <div className="input-outline py-5 px-7 my-4"><input type="email" name="email" id="email" {...register('email',{required:true})} placeholder="Email Address" className="w-full" /></div>
+                            {errors.email && <span className='error-color'>Enter Your Email</span>}
                         </div>
                         <div className="full-fields flex justify-between">
                             <div className='w-full mr-5'>
                                 <span className="gray">Birth Date</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="date" name="birthdate" id="birthdate" placeholder="Birth Date" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="date" name="birthdate" id="birthdate" {...register('birthDate',{required:true,valueAsDate: true,})} placeholder="Birth Date" className="w-full" /></div>
+                                {errors.birthDate && <span className='error-color'>Enter Your Birth Date</span>}
                             </div>
                             <div className='w-full ml-5'>
                                 <span className="gray">Phone Number</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="number" name="phonenumber" id="phonenumber" placeholder="Phone Number" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="number" name="phonenumber" id="phonenumber" {...register('phoneNumber',{required:true})} placeholder="Phone Number" className="w-full"  /></div>
+                                {errors.phoneNumber && <span className='error-color'>Enter Your Phone Number</span>}
                             </div>
                         </div>
                         <div className="full-fields flex justify-between">
                             <div className='w-full mr-5'>
                                 <span className="gray">Province</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="text" name="province" id="province" placeholder="Province" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="text" name="province" id="province" {...register('province',{required:true})} placeholder="Province" className="w-full"  /></div>
+                                {errors.province && <span className='error-color'>Enter Your Province Name</span>}
                             </div>
                             <div className='w-full ml-5'>
                                 <span className="gray">City</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="text" name="text" id="city" placeholder="City" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="text" name="city" id="city" {...register('city',{required:true})} placeholder="City" className="w-full"  /></div>
+                                {errors.city && <span className='error-color'>Enter Your City Name</span>}
                             </div>
                         </div>
                         <div>
                             <span className="gray">Full Address</span>
-                            <div className="input-outline py-5 px-7 my-4"><textarea placeholder="Full Address" cols="12" rows="2" className="w-full" /></div>
+                            <div className="input-outline py-5 px-7 my-4"><textarea name='fullAddress' id='fullAddress' {...register('fullAddress',{required:true})} placeholder="Full Address" cols="12" rows="2" className="w-full"  /></div>
+                            {errors.fullAddress && <span className='error-color'>Enter Your Full Address</span>}
+                        </div>
+                        <div>
+                            <span className="gray">Profile Picture</span>
+                            <div className="input-outline py-5 px-7 my-4"><input type="file" name="profileImage" id="profileImage" {...register('profileImage',{required:true})} placeholder="profileImage" className="w-full"  /></div>
+                            {errors.profileImage && <span className='error-color'>Select Your Profile Image</span>}
                         </div>
                         <div className="full-fields flex justify-between">
                             <div className='w-full mr-5'>
                                 <span className="gray">Password</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="password" name="password" id="password" placeholder="Password" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="password" name="password" id="password" {...register('password',{required:true})} placeholder="Password" className="w-full"  /></div>
+                                {errors.password && <span className='error-color'>Enter Your Password</span>}
                             </div>
                             <div className='w-full ml-5'>
                                 <span className="gray">Confirm Password</span>
-                                <div className="input-outline py-5 px-7 my-4"><input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm Password" className="w-full" /></div>
+                                <div className="input-outline py-5 px-7 my-4"><input type="password" name="confirmPassword" id="confirmPassword" {...register('confirmPassword',{required:true,validate:(val)=>{
+                                    if(watch('password')!=val){
+                                        return "Your passwords do no match";
+                                    }
+                                }})} placeholder="Confirm Password" className="w-full" /></div>
+                                {errors.confirmPassword && <span className='error-color'>Enter Correct Password</span>}
                             </div>
                         </div>
-                        <button className="text-white w-full py-5 my-4">Sign Up</button>
+                        <button type='submit' className="text-white w-full py-5 my-4">Sign Up</button>
                     </form>
                 </div>
             </div>
@@ -70,5 +158,6 @@ export default function SignUp() {
                 </div>
             </div>
         </div>
+        </>
     )
 }
---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getStorage} from 'firebase/storage'
+import {getFirestore} from 'firebase/firestore/lite'
+import {getAuth} from 'firebase/auth'
+const firebaseConfig = {
+  apiKey: "AIzaSyAESK95xl-SjbMN3WJoUIm5kVeBYTwXAdU",
+  authDomain: "bank-admin-dashboard.firebaseapp.com",
+  databaseURL: "https://bank-admin-dashboard-default-rtdb.firebaseio.com",
+  projectId: "bank-admin-dashboard",
+  storageBucket: "bank-admin-dashboard.appspot.com",
+  messagingSenderId: "205727495951",
+  appId: "1:205727495951:web:75c60af84eb4ee7b2eb07a",
+  measurementId: "G-E3JWY72JPF"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+export const db = getFirestore(app)
+export const auth = getAuth(app);
